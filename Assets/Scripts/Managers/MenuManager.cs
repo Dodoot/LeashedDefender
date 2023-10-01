02378db2f690a94ee433dafcd32e8bacaec3f51e
Dodoot <63876551+Dodoot@users.noreply.github.com>
2023-10-01 23:26:47 +0200

    enable spacebar in menus

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,10 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
     public void OnButtonPlayClicked()
     {
         SceneManager.LoadScene("Game");
